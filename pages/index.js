1d91bbb8d5e2fb4c5f74ffffcf91e36b4950d632
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-05 02:34:16 +0100

    removed voting drawer component

--- pages/index.js
+++ pages/index.js
@@ -63,9 +63,6 @@ const Update = () => {
         <Header />
         <div id="home" className="bg-[url('/whale__three.gif')] bg-center bg-cover bg-no-repeat h-[80vh] lg:h-screen items-center justify-center">
         <motion.div
-        // variants={headerVariant}
-        // initial="hidden"
-        // animate="visible"
         className="lg:pt-[40vh] pt-[30vh] lg:w-[95vw] text-white text-[25px] w-[80vw] mx-auto lg:text-[60px] justify-center font-mono items-center flex flex-col text-center">
             <h2>Saving the <span className="text-teal-600">whales</span>,</h2>
             <span>one <span className="text-teal-600">transaction</span> at a time</span>
@@ -335,93 +332,6 @@ const Update = () => {
         </motion.div>
     </motion.div>       
      </motion.div>
-
-
-    {/* <div className="roadmap flex items-center justify-center lg:w-[95vw] pt-[10vh]">
-           <span className="text-white text-[20px] lg:text-[40px] font-mono font-semibold">Whexcosystem Roadmap - 2022</span>      
-        </div>
-        <div className="roadmap-cards grid grid-cols-1 lg:grid-cols-4 lg:space-y-0 space-y-10 lg:space-x-4 pt-[5vh] w-[90vw] lg:w-[95vw] mx-auto">
-            <div className="border border-gray-800 px-4 py-6 text-white">
-              <span className="text-[30px] font-mono font-bold text-gray-100">Q1</span>
-              <div className="flex flex-col space-y-3 mt-10">
-                <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-              </div>
-            </div>
-            <div className="border border-gray-800 px-4 py-6 text-white">
-              <span className="text-[30px] font-mono font-bold text-gray-100">Q2</span>
-              <div className="flex flex-col space-y-3 mt-10">
-                <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-              </div>
-            </div>
-            <div className="border border-gray-800 px-4 py-6 text-white">
-              <span className="text-[30px] font-mono font-bold text-gray-100">Q3</span>
-              <div className="flex flex-col space-y-3 mt-10">
-                <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-              </div>
-            </div>
-            <div className="border border-gray-800 px-4 py-6 text-white">
-              <span className="text-[30px] font-mono font-bold text-gray-100">Q4</span>
-              <div className="flex flex-col space-y-3 mt-10">
-                <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-                  <span className="text-[15px] font-mono font-semibold text-gray-400">
-                    {"<>"} Launch of the Whale Explorer
-                  </span>
-              </div>
-            </div>
-        </div> */}
 
         <motion.div
           initial={{ opacity: 0, scale: 0.75 }}
