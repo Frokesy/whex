9d8509408679387b626f6d672b5d7dcacccf5567
Frokes <akindeleayanfeoluwa@gmail.com>
2022-12-03 02:24:03 +0100

    refactored

---
+++ components/Whexdao.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState, useRef } from 'react'
+import { motion } from 'framer-motion'
+
+const Whexdao = () => {
+  const [isDaoVisible, setIsDaoVisible] = useState(false)
+  return (
+    <motion.div 
+    layout
+    transition={{ layout: { duration: 0.5 } }}
+    className="border border-gray-800 px-4 py-6 text-white"
+    id="whexdao"
+    >
+    <div className="flex flex-row space-x-2 justify-center items-center">
+        <img src="/logo.jpg" alt="whexdao-logo" className="rounded-full lg:w-[4rem] md:w-[3rem] w-[3rem]" />
+        <span className="lg:text-[25px] text-[22px] font-mono font-bold text-white">whexcosystem DAO (whexdao)</span>
+    </div>
+    <div className="flex flex-col mx-4 space-y-3 mt-6">
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Network: Binance Smart Chain
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Address: 0x1fFD93F8c3d99bDAA4A012A0Fc26868F00Cfa5c2
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Symbol: $whexdao
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Decimals: 9
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Initial Supply: 1,000,000,000 WHEXDAO
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Stockpile Address: 0x0D7079098C8feb4F4AEF40703CE83eCc704f371d
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Private Sales: 200,000,000
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Presale(dxscale): 250,000,000
+                </span>
+            </div>
+            {isDaoVisible && (
+                <motion.div
+                initial={{ 
+                    y: 0,
+                    opacity: 0,
+                 }}
+                animate={{ 
+                    y: 15,
+                    opacity: 1, 
+                }}
+                transition={{ 
+                    duration: 0.5
+                 }}
+                >
+            <div className="flex flex-col mx-4 space-y-3">
+            <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Airdrop to WHEX holders (on completion of presale): 100,000,000
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Liquidity Pool: 250,000,000
+                </span>
+                <span className="text-[14px] font-mono font-semibold text-gray-400">
+                    {">>"} Airdrop to WHEXy Whales (on mint out): 199,995,591
+                </span>
+            <h2 className="flex text-gray-100 pt-8 text-[20px] font-mono font-bold">Voting Mechanism</h2>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {">>"}A voting cycle is initiated by Peter Wake, or by someone sending 4800 WHEXDAO tokens to the stockpile address and proposing a question to Peter Wake, who will then set up the voting addresses.
+                </span>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {">>"}  WHEXDAO tokens are sent out from the Stockpile to holders of WHEXy Whales at a rate of 1 WHEXDAO per 1 WHEXy Whale.
+                </span>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {">>"} Peter Wake will then publish a question in discord, Twitter and telegram and provide directions on what address to send WHEXDAO tokens to for each option.
+                </span>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {">>"} The voting period will end 42 hours after the vote goes live. All WHEXDAO tokens held by the vote option. addresses will be sent back to the stockpile.
+                </span>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {">>"} The decision will be carried out by the WHEXcosystem team. (Must have at least 51% of the total WHEXDAO used to vote).
+                </span>
+                <span className="text-[12px] font-mono font-semibold text-gray-400">
+                    {"Note"} As CEO, Peter Wake retains the right to veto any decision, or decide not to run a vote on a question presented by the community. I.E. you cannot use the DAO to perform a hostile takeover of the WHEX contract, decide to perform illegal acts, etc.
+                </span>
+                
+            </div>
+                </motion.div>
+            )}
+                <div 
+                className="flex justify-end"
+                onClick={() => setIsDaoVisible(!isDaoVisible)}
+                >
+                    {isDaoVisible ? (
+                      <button className="bg-teal-800 mt-8 font-light text-white rounded-lg font-mono text-[11px] px-6 py-4">Click to hide</button>
+                    ) : (
+                        <button className="bg-teal-800 mt-8 font-light text-white rounded-lg font-mono text-[11px] px-6 py-4">Click to expand</button>
+                    )}
+                </div>
+</motion.div>
+  )
+}
+
+export default Whexdao
---
+++ components/WhexyWhales.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState, useRef } from 'react'
+import { motion } from 'framer-motion'
+
+const WhexyWhales = () => {
+    const [isWhexyVisible, setIsWhexyVisible] = useState(false)
+  return (
+    <div>
+       <motion.div 
+        layout
+        transition={{ layout: { duration: 0.5 } }}
+        className="border border-gray-800 px-4 py-6 text-white"
+        id="whexy"
+        >
+            <div className="flex flex-row space-x-2 justify-center items-center">
+                <img src="/whexy.png" alt="whexy-logo" className="rounded-full lg:w-[4rem] md:w-[3rem] w-[3rem]" />
+                <span className="text-[25px] font-mono font-bold text-white">WHEXy whales (NFT)</span>
+            </div>
+            <span className="flex justify-center text-center text-neutral-400 mt-4 text-[13px] font-normal">WHEXy Whales is an NFT project consisting of 4800 unique Whales with different traits like hats, glasses, shirts, backgrounds, etc</span>
+            <div className="flex flex-col mx-4 space-y-3 mt-4 text-neutral-400">
+                        <span className="text-[18px] font-mono font-bold">
+                            Minting &amp; Royalties (How they will be used):
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold">
+                            {">>"} 25% to whale conservation charities 
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold">
+                            {">>"} 25% buys WHEX for WHEXDAO staking pools.
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold">
+                            {">>"} 25% to marketing and administrative costs 
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold">
+                            {">>"} 25% founder pay 
+                        </span>
+                        <span className="text-[14px] font-semibold ">
+                            {">>"} Transacted after each 100 mints, or for royalties, each $1000 accumulated 
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold">
+                            {">>"} Mint Link: {" "}
+                            <a href="https://app.niftykit.com/drops/whexy" 
+                            target="_blank" rel="noreferrer" className="text-teal-600 font-light">
+                            https://app.niftykit.com/drops/whexy
+                            </a>    
+                        </span>
+                        {isWhexyVisible && (
+                        <motion.div
+                        initial={{ 
+                            y: 0,
+                            opacity: 0,
+                         }}
+                        animate={{ 
+                            y: 15,
+                            opacity: 1, 
+                        }}
+                        transition={{ 
+                            duration: 0.5
+                         }}
+                        >
+                         <div className="flex flex-col mx-4 space-y-3 mt-4">
+                         <span className="text-[18px] font-mono font-bold  ">
+                            Why mint a WHEXy whale today?
+                        </span>
+                     <span className="text-[14px] font-mono font-semibold  ">
+                            {"1"} Help real life whales
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold  ">
+                            {"2"} get airdropped WHEXcosystem DAO tokens and participate in determining the future of the project
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold  ">
+                            {"3"} Participate in mint out rewards:
+                        </span>
+                        <div className="flex flex-col py-2 text-[12px] font-mono font-normal  ">
+                            <span>
+                              {">>"} 1916-2099: 0.275 ETH 
+                            </span>
+                            <span>
+                              {">>"} 2100-2399: Typical Tiger NFT
+                            </span>
+                            <span>
+                              {">>"} 2400-2999: 0.55 ETH
+                            </span>
+                            <span>
+                              {">>"} 3000-3599: Apocalyptic Ape NFT
+                            </span>
+                            <span>
+                              {">>"} 3600-4199: 0.825 ETH
+                            </span>
+                            <span>
+                              {">>"} 4200-4799: Lazy Lion NFT
+                            </span>
+                        </div>
+                        <span className="text-[14px] font-mono font-semibold  ">
+                            {"4"} Access to getting a custom ENS subdomain of WHEXy.eth
+                        </span>
+                        <span className="text-[14px] font-mono font-semibold  ">
+                            {"5"} Access to an exclusive discord chat
+                        </span>
+                       </div>
+                      </motion.div>
+                    )}
+                        <div 
+                        className="flex justify-end"
+                        onClick={() => setIsWhexyVisible(!isWhexyVisible)}
+                        >
+                            {isWhexyVisible ? (
+                              <button className="bg-sky-900 mt-8 font-light text-white rounded-lg font-mono text-[11px] px-6 py-4">Click to hide</button>
+                            ) : (
+                                <button className="bg-sky-900 mt-8 font-light text-white rounded-lg font-mono text-[11px] px-6 py-4">Click to expand</button>
+                            )}
+                        </div>
+                    </div>
+        </motion.div>
+    </div>
+  )
+}
+
+export default WhexyWhales
