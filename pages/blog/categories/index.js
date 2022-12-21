8cea449fb43707b97a59d3e2da191ad904cddb2b
Frokes <akindeleayanfeoluwa@gmail.com>
2022-12-21 01:16:27 +0100

    designed cover screen

---
+++ pages/blog/categories/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>
+      <div className="flex justify-between w-[80vw] mx-auto">
+        <div className="bg-white p-3 rounded-lg"></div>
+        <div className="bg-white p-3 rounded-lg"></div>
+        <div className="bg-white p-3 rounded-lg"></div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Categories
