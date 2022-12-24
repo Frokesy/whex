dc425af7c3ac2d69a66859a8ddf5dd0472ec3049
Frokes <akindeleayanfeoluwa@gmail.com>
2022-12-24 23:46:03 +0100

    started implementing backend

--- pages/blog/auth/login.js
+++ pages/blog/auth/login.js
@@ -1,17 +1,76 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaGoogle } from 'react-icons/fa'
 import Header from '../../../components/blog/Header'
 import Meta from '../../defaults/Meta'
 import { supabase } from '../../../utils/supabaseClient'
 const Login = () => {
 
-  //check if supabase connected successfully
-    if (supabase) {
-      console.log("Connected successfully")
-    } else {
-      console.log("Error in connection")
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState({
+    error: '',
+    success: '',
+  })
+
+   const handleLogin = async (e) => {
+    e.preventDefault()
+    if (!email || !password) {
+      setMessage({
+        error: 'Please fill in all fields',
+        success: '',
+      })
+      setTimeout(() => {
+        setMessage({
+          error: '',
+          success: '',
+        })
+      }, 3000)
     }
+    try {
+      const { user, error } = await supabase.auth.signIn({
+          email: email,
+          password: password
+      })
+      if (error) {
+          setMessage({
+              error: error.message,
+              success: '',
+          })
+          setTimeout(() => {
+              setMessage({
+                  error: '',
+                  success: '',
+              })
+          }, 4000)
+          return
+      }
+      setMessage({
+          error: '',
+          success: "Login successful, you'll be redirected shortly",
+      })
+      // setTimeout(() => {
+      //     router.push('/chats')
+      //     setMessage({
+      //         error: '',
+      //         success: '',
+      //     })
+      // }, 3000)
+  } catch (error) {
+      setMessage({
+          error: error.message,
+          success: '',
+      })
+      console.log(error.message)
+      setTimeout(() => {
+          setMessage({
+              error: '',
+              success: '',
+          })
+      }, 4000)
+  }
+   }
+
   return (
     <div className="bg-[url('/blog/cover.png')] bg-center bg-cover bg-no-repeat items-center h-screen opacity-90 justify-center">
       <Meta />
@@ -20,14 +79,20 @@ const Login = () => {
         <div className="flex flex-col w-[30vw] mx-auto pt-10 bg-neutral-800 rounded-lg px-6">
           <h2 className="text-[30px] font-bold text-white">Sign In</h2>
           <span className="text-[#ccc] text-[13px]">For administrative use only!</span>
-          <form>
+          <p 
+          className={`${ message.error && 'text-red-700' } ${ message.success && 'text-[#0fa84e]' } p-1 text-[13px] font-semibold mt-4 rounded-md text-center`}>
+            {message.error ? message.error : message.success}
+          </p>
+          <form onSubmit={handleLogin}>
                         <div className="input-field flex flex-col space-y-2 mt-6">
                             <label htmlFor="email" 
                                 className="text-[#ccc] font-semibold">
                                     Email:
                             </label>
-                            <input type="email" name="email" id="email" 
-                                placeholder="johndoe@gmail.com"
+                            <input type="text" name="username" id="username" 
+                                placeholder="ayanfeoluwaakindele24@gmail.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] placeholder:italic bg-transparent outline-none outline-offset-2 text-[#ccc]"
                             />
                         </div>
@@ -38,6 +103,8 @@ const Login = () => {
                             </label>
                             <input type="password" name="password" id="password" 
                                 placeholder="********"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] outline-none bg-transparent outline-offset-2 text-[#ccc]" 
                             />
                         </div>
--- pages/blog/auth/register.js
+++ pages/blog/auth/register.js
@@ -1,18 +1,94 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaGoogle, FaGooglePlusG, FaGooglePlusSquare } from 'react-icons/fa'
 import Header from '../../../components/blog/Header'
 import Meta from '../../defaults/Meta'
+import { supabase } from '../../../utils/supabaseClient'
 
 const Signup = () => {
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [message, setMessage] = useState({
+        error: '',
+        success: '',
+      })
+
+    const handleSignup = async (e) => {
+        e.preventDefault()
+        if (!firstname || !lastname || !email || !password) {
+            setMessage({
+                error: 'Please fill in all fields',
+                success: '',
+            })
+            setTimeout(() => {
+                setMessage({
+                    error: '',
+                    success: '',
+                })
+            }, 3000)
+        }
+        try {
+            const { error } = await supabase.auth.signUp({
+                email: email,
+                password: password,
+            })
+            if (error) {
+                setMessage({
+                    error: error.message,
+                    success: '',
+                })
+                setTimeout(() => {
+                    setMessage({
+                        error: '',
+                        success: '',
+                    })
+                }, 3000)
+            }
+            else {
+                setMessage({
+                    error: '',
+                    success: 'Account created successfully! Please check your email to verify your account.',
+                })
+                setTimeout(() => {
+                    setMessage({
+                        error: '',
+                        success: '',
+                    })
+                }, 3000)
+            }
+    
+            const { data, error: authError } = await supabase.from("writers").insert([
+                {
+                    firstname: firstname,
+                    lastname: lastname,
+                    email: email,
+                }
+            ])
+            if (authError) {
+                console.log('error', authError)
+            }
+            else {
+                console.log('data', data)
+            }
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
+
   return (
     <div className="bg-[url('/blog/cover.png')] bg-center bg-cover bg-no-repeat items-center h-[full] opacity-90 justify-center">
       <Meta />
       <Header />
-      <div className="pt-[20vh] pb-[10vh]">
+      <div className="pt-[20vh] pb-[20vh]">
         <div className="flex flex-col w-[40vw] mx-auto pt-10 bg-neutral-800 rounded-lg px-6">
           <h2 className="text-[30px] font-bold text-white">Sign Up</h2>
-          <form>
+          <p 
+          className={`${ message.error && 'text-red-700' } ${ message.success && 'text-[#0fa84e]' } p-1 text-[13px] font-semibold mt-4 rounded-md text-center`}>
+            {message.error ? message.error : message.success}
+          </p>
+          <form onSubmit={handleSignup}>
                         <div className="input-field flex flex-col space-y-2 mt-6">
                             <label htmlFor="name" 
                                 className="text-[#ccc] font-semibold">
@@ -21,10 +97,14 @@ const Signup = () => {
                         <div className="flex space-x-2 w-[100%]">
                             <input type="text" name="firstname" id="firstname" 
                                 placeholder="John"
+                                value={firstname}
+                                onChange={(e) => setFirstname(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 w-[50%] focus:border focus:border-[#ccc] placeholder:italic bg-transparent outline-none outline-offset-2 text-[#ccc]"
                             />
                             <input type="text" name="lastname" id="lastname" 
                                 placeholder="Doe"
+                                value={lastname}
+                                onChange={(e) => setLastname(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 focus:border w-[50%] focus:border-[#ccc] placeholder:italic bg-transparent outline-none outline-offset-2 text-[#ccc]"
                             />
                         </div>
@@ -36,6 +116,8 @@ const Signup = () => {
                             </label>
                             <input type="email" name="email" id="email" 
                                 placeholder="johndoe@gmail.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] placeholder:italic bg-transparent outline-none outline-offset-2 text-[#ccc]"
                             />
                         </div>
@@ -46,16 +128,8 @@ const Signup = () => {
                             </label>
                             <input type="password" name="password" id="password" 
                                 placeholder="********"
-                                className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] outline-none bg-transparent outline-offset-2 text-[#ccc]" 
-                            />
-                        </div>
-                        <div className="input-field flex flex-col space-y-2 mt-6">
-                            <label htmlFor="password" 
-                                className="text-[#ccc] required:border-red-500 font-semibold">
-                                   Confirm Password:
-                            </label>
-                            <input type="password" name="password" id="password2" 
-                                placeholder="********"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 className="border-gray-500 border-2 rounded-md p-3 focus:border focus:border-[#ccc] outline-none bg-transparent outline-offset-2 text-[#ccc]" 
                             />
                         </div>
