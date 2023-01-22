4cd903da6bb2143d725d1385839021cf8c55ea81
Frokes <akindeleayanfeoluwa@gmail.com>
2023-01-22 17:26:00 +0100

    designed mobile screen for featured category

--- components/blog/Footer.jsx
+++ components/blog/Footer.jsx
@@ -6,19 +6,19 @@ const Footer = () => {
         <div className="pt-[20vh] text-white flex items-center justify-center">
             <div className="flex items-center justify-center flex-col">
                 <h2 className="lg:text-[40px] text-[30px] text-center font-bold font-mono">Community</h2>
-                <div className="flex lg:space-x-10 space-x-4">
+                <div className="flex space-x-10">
                 <a href="https://twitter.com/whaleexploder">
-                <img src="/twitter.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 mx-8" />
+                <img src="/twitter.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 lg:mx-8" />
                 </a>
                 <a href="https://t.me/WHEXcosystem">
-                <img src="/telegram.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 mx-8" />
+                <img src="/telegram.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 lg:mx-8" />
                 </a>
                 <a 
                 href="https://www.reddit.com/r/WHEX/">
-                <img src="/reddit.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 mx-8" />
+                <img src="/reddit.svg" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 lg:mx-8" />
                 </a>
-                <a href="https://bscscan.com/token/0x31e79A378fb85D8c4d51489E5c90ecAF9c17935A"><img src="/bscscan.png" alt="" className="bg-white lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] rounded-full my-4 mx-5" /></a>
-                <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x31e79A378fb85D8c4d51489E5c90ecAF9c17935A"><img src="/pcs.png" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 mx-5" /></a>
+                <a href="https://bscscan.com/token/0x31e79A378fb85D8c4d51489E5c90ecAF9c17935A"><img src="/bscscan.png" alt="" className="bg-white lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] rounded-full my-4 lg:mx-5" /></a>
+                <a href="https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x31e79A378fb85D8c4d51489E5c90ecAF9c17935A"><img src="/pcs.png" alt="" className="lg:w-[4rem] lg:h-[4rem] w-[2rem] h-[2rem] my-4 lg:mx-5" /></a>
                 </div>
             </div>
             </div>
--- components/blog/categories/Featured.jsx
+++ components/blog/categories/Featured.jsx
@@ -18,33 +18,33 @@ const Featured = () => {
         }, [])
   return (
     <div>
-          <div className="pt-[10vh] w-[85vw]">
+          <div className="pt-[10vh] lg:w-[85vw] w-[90vw] mx-auto">
             <h2 className="text-white text-[30px]">Featured Posts</h2>
 
-            <div className="pt-6 grid grid-cols-2 gap-x-10">
+            <div className="pt-6 grid lg:grid-cols-2 grid-cols-1 gap-x-10">
                 <div className="flex flex-col space-y-4 mt-8 items-center">
                 {featuredArticles?.map((article, index) => (
-                    <div key={index} className="flex bg-white">
-                        <div className="w-[30%]">
-                            <img src={article.coverPhoto.url} alt={article.slug} className="h-[100%] object-cover" />
+                    <div key={index} className="flex bg-white lg:flex-row rounded-t-3xl lg:rounded-t-none flex-col">
+                        <div className="lg:w-[30%] w-[100%] h-[40vh] lg:h-auto">
+                            <img src={article.coverPhoto.url} alt={article.slug} className="h-[100%] w-[100%] object-cover" />
                         </div>
-                        <div className="flex flex-col space-y-2 px-4 rounded-r-lg">
-                            <h2 className="pt-6 text-[20px] font-bold">{article.title}</h2>
+                        <div className="flex flex-col space-y-2 px-3 rounded-r-lg">
+                            <h2 className="pt-6 text-[20px] font-bold cursor-pointer hover:underline">{article.title}</h2>
                             <span className="text-[14px]">{article.excerpt}</span>
                             
                             <div className="flex pb-6 pt-2">
                                 <div className="flex space-x-2">
-                                    <img src="/peter_wake.jpg" alt="image1" className="w-[14%] rounded-full" />
+                                    <img src={article.author.avatar.url} alt="image1" className="w-[3rem] h-[2.2rem] rounded-full" />
                                     <div className="flex flex-col">
-                                        <span className="font-bold text-[15px]">{article.author.name}</span>
-                                        <span className="text-[11px]">
+                                        <span className="font-semibold lg:text-[13px] text-[11px]">Peter Wake</span>
+                                        <span className="lg:text-[11px] text-[11px]">
                                             {moment(article.createdAt).format('MMMM Do YYYY')}
                                         </span>
                                     </div>
                                 </div>
                                 <div className="flex justify-end w-full">
                                     <Link href={`/blog/${article.slug}`} passHref>
-                                    <button className="bg-blue-400 font-light text-white rounded-lg text-[12px] px-4 py-2">Read More</button>
+                                    <button className="bg-blue-400 font-light text-white rounded-lg text-[12px] px-4 py-1 lg:py-2">Read More</button>
                                     </Link>
                                 </div>
                             </div>
@@ -54,9 +54,9 @@ const Featured = () => {
                 </div>
 
 
-                <div className="flex flex-col bg-white rounded-t-3xl">
+                <div className="flex flex-col lg:mt-0 mt-6 bg-white rounded-t-3xl">
                         <img src={mainFeaturedArticle?.coverPhoto?.url} alt="image1" className="object-fit h-[40vh] w-[100%]" />
-                    <h2 className="pt-4 px-6 font-bold">{mainFeaturedArticle?.title}</h2>
+                    <h2 className="pt-4 px-6 font-bold cursor-pointer hover:underline">{mainFeaturedArticle?.title}</h2>
                     <span className="pb-4 pt-2 px-6 text-[13px]">{mainFeaturedArticle?.excerpt}</span>
                     <div className="flex justify-end pb-3 pt-1 px-6">
                         <Link href={`/blog/${mainFeaturedArticle.slug}`} passHref>
