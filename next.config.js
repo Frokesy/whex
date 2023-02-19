4816aafb0e56698f238a12a56c72234352d2722b
Frokes <akindeleayanfeoluwa@gmail.com>
2023-02-19 03:56:20 +0100

    added mintme verification page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    async rewrites() {
+        return [{
+            source: '/mintme',
+            destination: '/html/mintme.html',
+        }, ]
+    }
 }
 
 module.exports = nextConfig
