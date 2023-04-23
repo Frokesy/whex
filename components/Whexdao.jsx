82e777099772d123259d0cf35d4f2b5df45b5b28
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-04-23 15:07:28 +0100

    updated whexdao

--- components/Whexdao.jsx
+++ components/Whexdao.jsx
@@ -14,6 +14,10 @@ const Whexdao = () => {
         <img src="/weed.png" alt="whexdao-logo" className="rounded-full lg:w-[4rem] md:w-[3rem] w-[3rem]" />
         <span className="lg:text-[25px] text-[18px] font-mono font-bold text-white">WHEXcosystem Exploder Ecosystem Daov2</span>
     </div>
+    <span className="flex justify-center text-neutral-400 mt-4 px-3 text-[13px] font-normal">The WHEXcosystem DAO ($WEED) token is used to make decisions on the future of the WHEXcosystem via blockchain vote. </span>
+    <span className="flex justify-center text-neutral-400 mt-2 px-3 text-[13px] font-normal">Proceeds and royalties of the WHEXy Whales:
+        <a href="https://app.niftykit.com/collections/drops/whexy" target="_blank" rel="noreferrer" className="text-blue-400 underline font-light">https://app.niftykit.com/collections/drops/whexy</a>   
+    </span>
     <div className="flex flex-col mx-4 space-y-3 mt-6 text-neutral-400">
                 <span className="text-[14px] font-mono font-semibold">
                     {">>"} Pancake Swap: {" "}
@@ -34,12 +38,6 @@ const Whexdao = () => {
                 <span className="text-[14px] font-semibold">
                     {">>"} Decimals: 18
                 </span>
-                <span className="text-[14px] font-semibold">
-                    {">>"} Staking: coming soon.
-                </span>
-                <span className="text-[14px] font-semibold">
-                    {">>"} Initial Supply: 1,000,000 WHEXDAO
-                </span>
             </div>
             {isDaoVisible && (
                 <motion.div
@@ -56,6 +54,12 @@ const Whexdao = () => {
                  }}
                 >
             <div className="flex flex-col mx-4 space-y-3 text-neutral-400">
+                <span className="text-[14px] font-semibold">
+                    {">>"} Staking: coming soon.
+                </span>
+                <span className="text-[14px] font-semibold">
+                    {">>"} Initial Supply: 1,000,000 WHEXDAO
+                </span>
                 <span className="text-[14px] font-semibold">
                     {">>"} Private Sales: 35,000
                 </span>
@@ -88,13 +92,16 @@ const Whexdao = () => {
                 </span>
             <h2 className="flex text-white pt-8 text-[18px] font-mono font-bold">Voting Mechanism</h2>
                 <span className="text-[12px] font-semibold text-neutral-400">
-                    {">>"} Holders will be able to vote soon, without having to send tokens to addresses using a dapp. This will allow holders to participate in the future of the WHEXcosystem without the need to give up voting power. 
+                    {">>"} Holders can vote without having to send tokens to addresses using a dapp. This will allow holders to participate in the future of the WHEXcosystem without the need to give up voting power. This is hosted at: 
+                    <a href="https://snapshot.org/#/whexy.eth" target="_blank" rel="noreferrer" className="text-blue-400 underline font-light">
+                        https://snapshot.org/#/whexy.eth
+                    </a>
                 </span>
                 <span className="text-[12px] font-semibold text-neutral-400">
-                    {">>"} The decision will be carried out by the WHEXcosystem team. (Must have at least 51% of the total $WEED used to vote).
+                    {">>"} The decision will be carried out by the WHEXcosystem team. (Must have at least 51% of the total <span className="text-blue-400 font-semibold">$WEED</span> used to vote).
                 </span>
                 <span className="text-[12px] font-semibold text-neutral-400">
-                    {">>"}  Voting on the $WEED tokenomics, what to do with the $WEED built up in DAO Treasury and $WHEX in the charity/burn wallet are allowed.
+                    {">>"}  Voting on the <span className="text-blue-400 font-semibold">$WEED</span> tokenomics, what to do with the <span className="text-blue-400 font-semibold">$WEED</span> built up in DAO Treasury and <span className="text-blue-400 font-semibold">$WHEX</span> in the charity/burn wallet are allowed.
                 </span>
                 <span className="text-[12px] font-semibold text-neutral-400">
                     {"Note"} As CEO, Peter Wake retains the right to veto any decision, or decide not to run a vote on a question presented by the community. I.E. you cannot use the DAO to perform a hostile takeover of the WHEX contract, decide to perform illegal acts, etc.
